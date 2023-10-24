fc5ed58d928ff022c478f4c089e1609e5d9cf4d3
Rainers Krumins <rainerkrum@gmail.com>
2023-10-24 16:32:17 +0300

    working logging for main.py and reddit.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-import reddit
-import db
+import logging
+import logging.config
+import yaml
 from datetime import datetime
 
+#setup logging
+with open("log-config.yaml") as stream:
+    logConfig = yaml.safe_load(stream)
 
+logging.config.dictConfig(logConfig)
+logger = logging.getLogger(__name__)
+
+#imports modules that have logging
+import reddit
+import db
+
+
+
+#start main sequence
+logger.info("Iniating new sequence.")
 
 countriesFile = open("countries.txt", "r")
 countries = countriesFile.read().split("\n")
@@ -14,11 +29,11 @@ dt = datetime.now()
 today = str(dt.year) + "-" + str(dt.month).zfill(2) + "-" + str(dt.day).zfill(2)
 
 
-#search articles for country names and populate db
-for article in articles:
-    for country in countries:
-        if country in article:
-            db.insertIntoMentions(today, country, article)
+# #search articles for country names and populate db
+# for article in articles:
+#     for country in countries:
+#         if country in article:
+#             db.insertIntoMentions(today, country, article)
 
 
 
--- reddit.py
+++ reddit.py
@@ -1,10 +1,36 @@
 import requests
 from configparser import ConfigParser
+import logging
 
 def getArticles():
-    # Initiating and reading config values
-    print("Loading configuration from file...")
+    global header
+    if header is None:
+        header = acquireHeader()
+    
     try:
+        logger.info("Requesting daily top posts from r/worldnews...")
+        res = requests.get("https://oauth.reddit.com/r/worldnews/top/?t=day", headers=header)
+        logger.info("SUCCESS")
+    except:
+        logger.exception("ERROR")
+
+    articles = []
+    for post in res.json()["data"]["children"]:
+        articles.append(post["data"]["title"].lower())
+
+    logging.debug(articles)
+
+    return articles
+
+def acquireHeader():
+    logger.info("Building header.")
+    #access token will be added to the headers and then functin will return it
+    header = {"User-Agent": "MyApi/0.0.1"}
+    
+    # reading config
+    try:
+        logger.info("Reading config file...")
+
         config = ConfigParser()
         config.read("config.ini")
 
@@ -12,36 +38,35 @@ def getArticles():
         USERNAME = config.get("auth.info", "username")
         SECRET_KEY = config.get("auth.info", "secret_key")
         PASSWORD = config.get("auth.info", "password")
+
+        logger.info("SUCCESS")
     except:
-        print("ERROR")
-
-    print("SUCCESS")
+        logger.exception("ERROR")
 
 
-    
-
-    #acquire request header
+    #acquire access token
     auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
     data = {
         "grant_type": "password",
         "username": USERNAME,
         "password": PASSWORD
     }
-    headers = {"User-Agent": "MyApi/0.0.1"}
+    try:
+        logger.info("Acquiring access token for reddit API...")
+        res = requests.post("https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=header)
+        TOKEN = res.json()["access_token"]
+        logger.info("SUCCESS")
+    except:
+        logger.exception("ERROR")
 
-    res = requests.post("https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=headers)
-    TOKEN = res.json()["access_token"]
+    #add access token to headers
+    header["Authorization"] = f'bearer {TOKEN}'
+    logger.info("Header built.")
+    logger.debug(f'Header: {header}')
 
-    headers["Authorization"] = f'bearer {TOKEN}'
+    return header
+    
 
 
-
-
-    #acquire data
-    res = requests.get("https://oauth.reddit.com/r/worldnews/top/?t=day", headers=headers)
-
-    artciles = []
-    for post in res.json()["data"]["children"]:
-        artciles.append(post["data"]["title"].lower())
-
-    return artciles
+header = None
+logger = logging.getLogger(__name__)
