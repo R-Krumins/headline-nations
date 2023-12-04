7c35b08fcea63be188f8108a76a16cafa93776a1
Rainers Krumins <rainerkrum@gmail.com>
2023-12-04 22:07:48 +0200

    scrapped data now saves to db

--- main.py
+++ main.py
@@ -29,11 +29,11 @@ dt = datetime.now()
 today = str(dt.year) + "-" + str(dt.month).zfill(2) + "-" + str(dt.day).zfill(2)
 
 
-# #search articles for country names and populate db
-# for article in articles:
-#     for country in countries:
-#         if country in article:
-#             db.insertIntoMentions(today, country, article)
+#search articles for country names and populate db
+for article in articles:
+    for country in countries:
+        if country in article:
+            db.insertIntoMentions(today, country, article)
 
 
 
